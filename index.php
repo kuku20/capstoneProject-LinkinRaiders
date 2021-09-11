0d0a2bbe27df61a47e8acf48abf758c1130d7d0b
kuku20 <lluu3790@gmail.com>
2021-09-11 10:49:00 -0500

    check connect to database

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
  echo "Hello canata";
- //echo phpinfo();
-//  $client = new MongoDB\Client("mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu?retryWrites=true&w=majority");
-
-// $db = $client->test;
- // $connection = new MongoDB\Driver\Manager("mongodb://localhost:27017");
- // $mng = new MongoDB\Driver\Manager("mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu?retryWrites=true&w=majority");
  //mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
   $mng = new MongoDB\Driver\Manager("mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu");
  $qry = new MongoDB\Driver\Query([]);
   $rows = $mng->executeQuery("datattu.products", $qry);
- //$rows = $mng->executeQuery("test.products", $qry);
- 
+
  foreach($rows as $document) {
     print_r($document);
 }
