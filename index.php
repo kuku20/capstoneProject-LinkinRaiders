c0fa15d5ed924386dd4cf5b9094e33a52ef03d0d
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-09-20 15:00:13 -0500

    use  MongoDB\Driver\Manager

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 require '../vendor/autoload.php';
  echo "Hello canata";
 
- $client = new MongoDB\Client(
-    'mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu'
-);
-$db = $client->test;
- echo "Connection to database successfully";
+//  $client = new MongoDB\Client(
+//     'mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu'
+// );
+// $db = $client->test;
+//  echo "Connection to database successfully";
 
+  $mng = new MongoDB\Driver\Manager("mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu");
+ $qry = new MongoDB\Driver\Query([]);
+  $rows = $mng->executeQuery("datattu.products", $qry);
+ //$rows = $mng->executeQuery("test.products", $qry);
+
+
+ foreach($rows as $document) {
+    print_r($document);
+}
 ?>
