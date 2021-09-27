8aab31ac38750d2df0dd8a1425ae2b27737ef075
Vincent Hew <vncent5779@gmail.com>
2021-09-27 00:03:44 -0500

    vin_front_end
    front end with side bar

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
-<?php
-require '../vendor/autoload.php';
- echo "Hello canata";
-
-//  $client = new MongoDB\Client(
-//     'mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu'
-// );
-// $db = $client->test;
-//  echo "Connection to database successfully";
-
-  $mng = new MongoDB\Driver\Manager("mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu");
- $qry = new MongoDB\Driver\Query([]);
-  $rows = $mng->executeQuery("datattu.products", $qry);
- //$rows = $mng->executeQuery("test.products", $qry);
-
-
- foreach($rows as $document) {
-    print_r($document);
-}
-?>
+<?php require('components/head.inc.php'); ?>
+<?php require('components/sideBar.inc.php'); ?>
+<div class="home_content">
+    <div class="text">Dashboard Content</div>
+</div>
+<?php require('components/footer.inc.php'); ?>
