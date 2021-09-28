618b970ccc39c9c2d96137d9b602fd8c03efa74a
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-09-28 00:33:41 -0500

    Delete web/vinWork directory

--- web/vinWork/components/logo.inc.php
+++
@@ -1,7 +0,0 @@
-        <div class="logo_content">
-            <div class="logo">
-                <i class='bx bxl-linkedin-square'></i>
-                <div class = "logo_name">LinkinRaider</div>
-            </div>
-            <i class='bx bx-menu' id = "btn"></i>
-        </div>
