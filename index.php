72f5a5597fbd02e83a0e290372aa180608287a21
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-09-28 00:34:35 -0500

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php require('components/head.inc.php'); ?>
-<?php require('components/sideBar.inc.php'); ?>
-<div class="home_content">
-    <div class="text">Dashboard Content</div>
-</div>
-<?php require('components/footer.inc.php'); ?>
