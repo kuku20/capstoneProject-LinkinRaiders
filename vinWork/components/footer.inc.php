cd8620a733ca3a8446fcd0ed3e43f7d8a52322a1
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-09-28 00:34:52 -0500

    Add files via upload

---
+++ vinWork/components/footer.inc.php
@@ -0,0 +1,10 @@
+<script>
+        let btn = document.querySelector("#btn");
+        let sidebar = document.querySelector(".sidebar");
+
+        btn.onclick = function() {
+            sidebar.classList.toggle("active")
+        }
+    </script>
+  </body>
+</html>
---
+++ vinWork/components/sideBar.inc.php
@@ -0,0 +1,5 @@
+<div class="sidebar">
+    <?php include('components/logo.inc.php'); ?>
+    <?php include('components/navList.inc.php'); ?>
+    <?php include('components/profile.inc.php'); ?>    
+</div>
