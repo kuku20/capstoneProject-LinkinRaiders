4b2c1eb699e5c6dc1d9465a40564fd49dcedccc8
Vincent Hew <vncent5779@gmail.com>
2021-10-17 20:02:31 -0500

    update side bar with link

---
+++ module/chatpage.php
@@ -0,0 +1,28 @@
+<?php 
+	require ('../vendor/autoload.php');
+ 	session_start();
+ 	$client = new MongoDB\Client('mongodb+srv://data2u:IMpSIo53MwLE0YwC@data2u.f9hzo.mongodb.net/datattu');
+	session_start(); 
+	if (!isset($_SESSION['username'])) {
+	  	$_SESSION['msg'] = "You must log in first";
+	  	header('location: ../index.php');
+	}
+	if ($_GET["logout"]== 1 AND $_SESSION['id']) {
+	  	session_destroy();
+	    header('location: ../index.php');
+	}
+	if (isset($_GET['logout'])) {
+	    session_destroy();
+	    unset($_SESSION['username']);
+	    header("location: ../index.php");
+		}
+ ?>
+
+<?php require('../components/inc/head.php'); ?>
+<?php require('../components/inc/sideBar.php'); ?>
+<div class="home_content">
+    <div class="text">Group matcher</div>
+    <h1 class="text">username: <?php echo $_SESSION['username'] ?></h1>
+    <h1 class="text">userID: <?php echo $_SESSION['id'] ?></h1>
+</div>
+<?php require('../components/inc/footer.php'); ?>
