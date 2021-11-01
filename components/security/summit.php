4cd6d973c6a26d0d377a6123bb4ad405256b9abe
Vincent Hew <vncent5779@gmail.com>
2021-11-01 09:16:30 -0500

    try fix conflict

--- components/security/summit.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	include( ROOT_PATH . '/vendor/autoload.php');
- 	$client = new MongoDB\Client('mongodb+srv://data2u:i8AohiQaOzxPEpIc@data2u.f9hzo.mongodb.net/datattu');
-
-
-
- ?>
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	include( ROOT_PATH . '/vendor/autoload.php');
- 	// session_start();
-	// session_start(['cookie_lifetime'=>2592000,]);
- 	$client = new MongoDB\Client('mongodb+srv://vin:PkWCp8BvSFNWNQ4e@data2u.f9hzo.mongodb.net/datattu');
-
-?>
