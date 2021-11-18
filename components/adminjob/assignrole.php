5442f4c3b73398c5fc875f536e92a75e9c6c7788
kuku20 <lluu3790@gmail.com>
2021-11-17 20:31:30 -0600

    fix sign role

--- components/adminjob/assignrole.php
+++ components/adminjob/assignrole.php
@@ -4,27 +4,12 @@
 	require_once ('../../session.php');
 
 	$collection = $client->datattu->ttu;
-	$insertOneResult = $collection->insertOne([
-    		'username' 	=> $_POST['userrole'],
-    		'name'		=> 'none',
-    		'email' 	=> $email,
-    		'gender'	=> 'none',
-    		'role'		=> 'none',
-    		'password' => $password_1,
-    		'google_secret'=>' ',
-    		// 'image'=>$image_data,
-    		'image'=>null,
-    		'google_require' => false,
-			]);
-
 	$_POST['userrole'];
 	if (isset($_POST['userrole'])) {
 		$updateResult =$collection->updateOne(  
 			['username' => $_POST['userrole']],
 			['$set' => ['role' => $_POST['role']]],
 		);
-
-		
 	    }
 
 
