8b345d24d0f2fe1bf579e49a17700dc89bb9fa58
vincent5779 <57268223+vincent5779@users.noreply.github.com>
2021-11-19 01:19:17 -0600

    Add files via upload

--- module/Translator.php
+++ module/Translator.php
@@ -1,43 +1,33 @@
-<?php 
-  require ('../vendor/autoload.php');
-  require_once ('../config.php');
-  require_once ('../session.php');  
-  require('../components/inc/head.php');
-  require('../components/inc/sideBar.php');
-  require('../components/inc/footer.php'); 
-
- ?>
+<php?
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en-US", dir ="ltr">
 <head>
   <meta charset="UTF-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
-  <meta name="viewport" content="width-device-width, initial-scale=1.0"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
   <!--Call the CSS files to make it like the template on Tailbocks-->
-  <link rel="stylesheet" href="first/assests/css/tailwind.min.css"/>
-  <link rel="stylesheet" href="first/assests/css/style.css"/>
-
-  <title>Language Translator</title>
-
+  <!-- Link to the Tailblocks site to help create my translator https://tailblocks.cc/ -->
+  <link rel="stylesheet" href="../assets/tailwind.min.css"/>
+  <title>LinkinRaider</title>
+  </head>
   <!-- This is code from Tailblocks known as Contact us that we will change to make our format-->
   <body class="bg-gray-900">
-    <!-- this for match vin css -->
-    <div class="home_content">
-      <div class="text">
-        
-
-      
-    <section class="text-gray-400 bg-gray-900 body-font relative">
+    <!-- bg is background-color-shading-->
+    <section class="text-gray-100 bg-gray-900 body-font relative">
       <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-col text-center w-full mb-12">
+          <!-- This is the text to establish to the user on how to use the translator-->
           <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Language Translator</h1>
           <p class="lg:w-2/3 mx-auto leading-relaxed text-base">This is our Senior Capstone Language Translator</p>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text base">Instructions: Type in the message box to have it translated. And choose between your two languages. Use the left to have clarify your initial language and sse the right for your desired language.</p>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text base">Note: It will automatically detect the language even if you chose the wrong initial language.</p>
         </div>
         <div class="lg:w-1/2 md:w-2/3 mx-auto">
           <div class="flex flex-wrap -m-2">
             <div class="p-2 w-1/2">
               <div class="relative">
+                <p class="lg:w-2/3 mx-auto leading-relaxed text base">Initial Language</p>
                 <!--This will be our first language that will detect the users defualt language and will give an option -->
                 <select type="text" id="lang_one" name="lang_one" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                   <option value="Auto_Detect">Auto Detect</option>
@@ -118,6 +108,7 @@
             </div>
             <div class="p-2 w-1/2">
               <div class="relative">
+                <p class="lg:w-2/3 mx-auto leading-relaxed text base">Desire Language</p>
                 <!--This will be the language option for when the translator will use to translate to -->
                 <select type="text" id="lang_two" name="lang_two" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                   <option value="EN">English</option>
@@ -205,47 +196,10 @@
             <div class="p-2 w-full">
               <button id="translate" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Translate</button>
             </div>
-            <div class="p-2 w-full pt-8 mt-8 border-t border-gray-800 text-center">
-              <!--Some business contact info that we can remove or change later-->
-              <a class="text-indigo-400">example@email.com</a>
-              <p class="leading-normal my-5">49 Smith St.
-                <br>Saint Cloud, MN 56301
-              </p>
-              <!-- <span class="inline-flex">
-                <a class="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a class="ml-4 text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a class="ml-4 text-gray-500">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                  </svg>
-                </a>
-                <a class="ml-4 text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span> -->
-            </div>
           </div>
         </div>
       </div>
-
-
-      </div>
-    </div>
     </section>
-
-    <!--Javascrit that we'll use to help translate-->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/app.js"></script>
-  </body>
-</head>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="../assets/js/app.js"></script>
+?>
