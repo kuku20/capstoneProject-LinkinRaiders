39cf7035cd5dd40c443b00f32dfd47c3b3ac02fd
kuku20 <35443377+kuku20@users.noreply.github.com>
2021-11-19 01:47:19 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,226 @@
 <?php 
-
-require_once ('config.php');
-require_once ('components/security/signup.php');
+require_once 'vendor/autoload.php';
  ?>
- <?php
-session_start();
-if($_GET["logout"]== 1 AND $_SESSION['id']) {
-    session_destroy();
-    $message = "You have been logged out. Have a nice day!";
-    header('location: index.php');
-}
-?>
- <!DOCTYPE html>
- <html>
- <head>
-    <title>LinkinRaiders</title>
-    <!-- Google Fonts  -->
-    <link rel="stylesheet" type="text/css" href="">
-
-    <div>
-        <div class="login">
-            <form action="index.php" method="post">
-                <h2>Login</h2>
-                <input type="text" name="username" placeholder="Username" required="">
-                <input type="password" name="password" placeholder="Password" required="">
-                <?php require_once ('components/security/login.php');?>
-                <button class="loginbtn" type="submit" name="user_login">Sign In</button>   
-            </form>
+<!DOCTYPE html>
+<html lang="en-US", dir ="ltr">
+<head>
+  <meta charset="UTF-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <!--Call the CSS files to make it like the template on Tailbocks-->
+  <!-- Link to the Tailblocks site to help create my translator https://tailblocks.cc/ -->
+  <link rel="stylesheet" href="tailwind.min.css"/>
+  <title>LinkinRaider</title>
+  </head>
+  <!-- This is code from Tailblocks known as Contact us that we will change to make our format-->
+  <body class="bg-gray-900">
+    <!-- bg is background-color-shading-->
+    <section class="text-gray-100 bg-gray-900 body-font relative">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-12">
+          <!-- This is the text to establish to the user on how to use the translator-->
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Language Translator</h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">This is our Senior Capstone Language Translator</p>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text base">Instructions: Type in the message box to have it translated. And choose between your two languages. Use the left to have clarify your initial language and sse the right for your desired language.</p>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text base">Note: It will automatically detect the language even if you chose the wrong initial language.</p>
         </div>
-    </div>
- </head>
- <body>
- 
- </body>
- </html>
-<!--  //login user -->
+        <div class="lg:w-1/2 md:w-2/3 mx-auto">
+          <div class="flex flex-wrap -m-2">
+            <div class="p-2 w-1/2">
+              <div class="relative">
+                <p class="lg:w-2/3 mx-auto leading-relaxed text base">Initial Language</p>
+                <!--This will be our first language that will detect the users defualt language and will give an option -->
+                <select type="text" id="lang_one" name="lang_one" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <option value="Auto_Detect">Auto Detect</option>
+                  <option value="AF">Afrikaans</option>
+                  <option value="SQ">Albanian</option>
+                  <option value="AR">Arabic</option>
+                  <option value="HY">Armenian</option>
+                  <option value="EU">Basque</option>
+                  <option value="BN">Bengali</option>
+                  <option value="BG">Bulgarian</option>
+                  <option value="CA">Catalan</option>
+                  <option value="KM">Cambodian</option>
+                  <option value="ZH">Chinese (Mandarin)</option>
+                  <option value="HR">Croatian</option>
+                  <option value="CS">Czech</option>
+                  <option value="DA">Danish</option>
+                  <option value="NL">Dutch</option>
+                  <option value="EN">English</option>
+                  <option value="ET">Estonian</option>
+                  <option value="FJ">Fiji</option>
+                  <option value="FI">Finnish</option>
+                  <option value="FR">French</option>
+                  <option value="KA">Georgian</option>
+                  <option value="DE">German</option>
+                  <option value="EL">Greek</option>
+                  <option value="GU">Gujarati</option>
+                  <option value="HE">Hebrew</option>
+                  <option value="HI">Hindi</option>
+                  <option value="HU">Hungarian</option>
+                  <option value="IS">Icelandic</option>
+                  <option value="ID">Indonesian</option>
+                  <option value="GA">Irish</option>
+                  <option value="IT">Italian</option>
+                  <option value="JA">Japanese</option>
+                  <option value="JW">Javanese</option>
+                  <option value="KO">Korean</option>
+                  <option value="LA">Latin</option>
+                  <option value="LV">Latvian</option>
+                  <option value="LT">Lithuanian</option>
+                  <option value="MK">Macedonian</option>
+                  <option value="MS">Malay</option>
+                  <option value="ML">Malayalam</option>
+                  <option value="MT">Maltese</option>
+                  <option value="MI">Maori</option>
+                  <option value="MR">Marathi</option>
+                  <option value="MN">Mongolian</option>
+                  <option value="NE">Nepali</option>
+                  <option value="NO">Norwegian</option>
+                  <option value="FA">Persian</option>
+                  <option value="PL">Polish</option>
+                  <option value="PT">Portuguese</option>
+                  <option value="PA">Punjabi</option>
+                  <option value="QU">Quechua</option>
+                  <option value="RO">Romanian</option>
+                  <option value="RU">Russian</option>
+                  <option value="SM">Samoan</option>
+                  <option value="SR">Serbian</option>
+                  <option value="SK">Slovak</option>
+                  <option value="SL">Slovenian</option>
+                  <option value="ES">Spanish</option>
+                  <option value="SW">Swahili</option>
+                  <option value="SV">Swedish </option>
+                  <option value="TA">Tamil</option>
+                  <option value="TT">Tatar</option>
+                  <option value="TE">Telugu</option>
+                  <option value="TH">Thai</option>
+                  <option value="BO">Tibetan</option>
+                  <option value="TO">Tonga</option>
+                  <option value="TR">Turkish</option>
+                  <option value="UK">Ukrainian</option>
+                  <option value="UR">Urdu</option>
+                  <option value="UZ">Uzbek</option>
+                  <option value="VI">Vietnamese</option>
+                  <option value="CY">Welsh</option>
+                  <option value="XH">Xhosa</option>
+                </select>
+              </div>
+            </div>
+            <div class="p-2 w-1/2">
+              <div class="relative">
+                <p class="lg:w-2/3 mx-auto leading-relaxed text base">Desire Language</p>
+                <!--This will be the language option for when the translator will use to translate to -->
+                <select type="text" id="lang_two" name="lang_two" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <option value="EN">English</option>
+                  <option value="AF">Afrikaans</option>
+                  <option value="SQ">Albanian</option>
+                  <option value="AR">Arabic</option>
+                  <option value="HY">Armenian</option>
+                  <option value="EU">Basque</option>
+                  <option value="BN">Bengali</option>
+                  <option value="BG">Bulgarian</option>
+                  <option value="CA">Catalan</option>
+                  <option value="KM">Cambodian</option>
+                  <option value="ZH">Chinese (Mandarin)</option>
+                  <option value="HR">Croatian</option>
+                  <option value="CS">Czech</option>
+                  <option value="DA">Danish</option>
+                  <option value="NL">Dutch</option>
+                  <option value="ET">Estonian</option>
+                  <option value="FJ">Fiji</option>
+                  <option value="FI">Finnish</option>
+                  <option value="FR">French</option>
+                  <option value="KA">Georgian</option>
+                  <option value="DE">German</option>
+                  <option value="EL">Greek</option>
+                  <option value="GU">Gujarati</option>
+                  <option value="HE">Hebrew</option>
+                  <option value="HI">Hindi</option>
+                  <option value="HU">Hungarian</option>
+                  <option value="IS">Icelandic</option>
+                  <option value="ID">Indonesian</option>
+                  <option value="GA">Irish</option>
+                  <option value="IT">Italian</option>
+                  <option value="JA">Japanese</option>
+                  <option value="JW">Javanese</option>
+                  <option value="KO">Korean</option>
+                  <option value="LA">Latin</option>
+                  <option value="LV">Latvian</option>
+                  <option value="LT">Lithuanian</option>
+                  <option value="MK">Macedonian</option>
+                  <option value="MS">Malay</option>
+                  <option value="ML">Malayalam</option>
+                  <option value="MT">Maltese</option>
+                  <option value="MI">Maori</option>
+                  <option value="MR">Marathi</option>
+                  <option value="MN">Mongolian</option>
+                  <option value="NE">Nepali</option>
+                  <option value="NO">Norwegian</option>
+                  <option value="FA">Persian</option>
+                  <option value="PL">Polish</option>
+                  <option value="PT">Portuguese</option>
+                  <option value="PA">Punjabi</option>
+                  <option value="QU">Quechua</option>
+                  <option value="RO">Romanian</option>
+                  <option value="RU">Russian</option>
+                  <option value="SM">Samoan</option>
+                  <option value="SR">Serbian</option>
+                  <option value="SK">Slovak</option>
+                  <option value="SL">Slovenian</option>
+                  <option value="ES">Spanish</option>
+                  <option value="SW">Swahili</option>
+                  <option value="SV">Swedish </option>
+                  <option value="TA">Tamil</option>
+                  <option value="TT">Tatar</option>
+                  <option value="TE">Telugu</option>
+                  <option value="TH">Thai</option>
+                  <option value="BO">Tibetan</option>
+                  <option value="TO">Tonga</option>
+                  <option value="TR">Turkish</option>
+                  <option value="UK">Ukrainian</option>
+                  <option value="UR">Urdu</option>
+                  <option value="UZ">Uzbek</option>
+                  <option value="VI">Vietnamese</option>
+                  <option value="CY">Welsh</option>
+                  <option value="XH">Xhosa</option>
+                </select>
+              </div>
+            </div>
+            <div class="p-2 w-full">
+              <div class="relative">
+                <!--This will be the message that we will translate based on what the user will type-->
+                <label for="message" class="leading-7 text-sm text-gray-400">Message</label>
+                <textarea id="message" name="message" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              </div>
+            </div>
+            <div class="p-2 w-full">
+              <button id="translate" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Translate</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+  <!-- <script src="../assets/js/app.js"></script> -->
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#translate").on("click", function() {
+        var lang_one =$("#lang_one").val();
+        var lang_two =$("#lang_two").val();
+        var text =$("#message").val();
+        
+        $.ajax({
+            url: "process.php",
+            type: "post",
+            data: { lang_one: lang_one, lang_two: lang_two, text: text},
+            success: function (status){
+               text = $("#message").val(status);
+            
+            }
+        })
+      
+    });
+})
+</script>
